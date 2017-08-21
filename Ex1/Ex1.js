c5b5ca182fce491e4e69a4d01266b3ffa3b935db
Jorge <jorge.david.zepmed@gmail.com>
2017-08-21 12:16:44 -0500

    Se agregan archivos y se inicia el priemr ex

---
+++ Ex1/Ex1.js
@@ -0,0 +1,3 @@
+alert("Funciona?");     //Madna un alerta al cargar la pagina
+document.write ("<h1> Bienvendo a javascript </h1>")
+console.log("Progrma terminado")  // manda un mesnaje a la consola del navegador 
