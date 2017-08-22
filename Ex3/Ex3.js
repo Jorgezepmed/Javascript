911f9a051fe29d168367249770d451e6f51097cb
Jorge <jorge.david.zepmed@gmail.com>
2017-08-22 16:46:21 -0500

    Ex3 The Story Making Game

---
+++ Ex3/Ex3.js
@@ -0,0 +1,11 @@
+//The Story Making Game
+// Habia una vez un niño llamado NOMBRE que le gustaba jugar JUEGO entonces se le aparecio un MIEDO y lloro hasta hacer la popo de  COMIDA que comio
+
+alert("Estas listo?");
+var nombre = prompt("Cual es tu nombre?");
+var juego = prompt("Tu juego favorito..");
+var miedo = prompt("A que le tienes miedo?");
+var comida =prompt("Que comiste hoy?");
+var mensaje = "Habia una vez un niño llamado " + nombre;
+mensaje += " que le gustaba jugar " + juego + " entonces se le aparecio un " + miedo + " lloro hasta hacer la popo de " +comida + " que habia comido hoy.";
+document.write(mensaje);
